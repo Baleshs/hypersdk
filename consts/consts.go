e528b9ff501f338dcc131210a79587d4fc00fbd1
Baleshs <163410956+Baleshs@users.noreply.github.com>
2024-04-15 23:30:36 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "spiderman"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "spider"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "SPD"
 )
 
 var ID ids.ID
